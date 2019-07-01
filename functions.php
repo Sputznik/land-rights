52cbf94bea5e64801afd4258804adcd7c3ee14dd
unknown <samvthom16@gmail.com>
2019-07-01 19:50:03 +0530

    Show only imp taxonomies in the admin columns

--- functions.php
+++ functions.php
@@ -4,3 +4,16 @@ add_action('wp_enqueue_scripts',function(){
 });
 
 include('cpt/laws.php');
+
+
+function wpse_80027_manage_columns($columns) {
+
+  $remove_taxonomies = array( 'machine-readable', 'gazetted-copy', 'lang', 'source', 'status', 'nature' );
+  foreach( $remove_taxonomies as $tax ){
+    unset( $columns[ 'taxonomy-'. $tax ] ); // prepend taxonomy name with 'taxonomy-'
+  }
+
+  return $columns;
+
+}
+add_filter('manage_edit-laws_columns', 'wpse_80027_manage_columns');
