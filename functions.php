4178e9cda15dddd75197180f1ba8d29a97aa99c4
unknown <samvthom16@gmail.com>
2019-07-02 15:07:31 +0530

    Refactor

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 add_action('wp_enqueue_scripts',function(){
   wp_enqueue_style('landrights-style', get_stylesheet_directory_uri().'/assets/css/land-rights.css', array('sp-core-style'), '1.0.0' );
 });
