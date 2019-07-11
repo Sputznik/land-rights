7bf55843fa9b4d0188949b688464494e85335f9a
unknown <samvthom16@gmail.com>
2019-07-11 12:20:23 +0530

    move functions to orbit wp

--- functions.php
+++ functions.php
@@ -23,38 +23,17 @@ add_filter('manage_edit-laws_columns', function( $columns ){
   return $columns;
 } );
 
-function get_term_count_by_query( $term, $query_atts ){
-
-  if( !isset( $query_atts['tax_query'] ) ){
-    $query_atts['tax_query'] = array();
-  }
-
-  array_push( $query_atts['tax_query'], array(
-    'taxonomy'  => $term->taxonomy,
-    'field'     => 'slug',
-    'terms'     => array( $term->slug )
-  ) );
-
-  $query_atts['fields'] = 'ids';
-  $query_atts['posts_per_page'] = -1;
-
-  $ps = get_posts( $query_atts );
-
-  if( count( $ps ) > 0 ){ return count( $ps ); }
-
-  return 0;
-
-
-}
 
 function getListOfTerms( $taxonomy, $posts, $query_atts ){
+
+  $orbit_wp = ORBIT_WP::getInstance();
 
   $terms_list = array();
 
   $terms = wp_get_object_terms( $posts,  $taxonomy );
 
   foreach( $terms as $term ){
-    array_push( $terms_list, $term->name . " (" . get_term_count_by_query( $term, $query_atts ) . ")" );
+    array_push( $terms_list, $term->name . " (" . $orbit_wp->get_term_count_by_query( $term, $query_atts ) . ")" );
   }
 
   return $terms_list;
@@ -64,27 +43,21 @@ function getListOfTerms( $taxonomy, $posts, $query_atts ){
 
 add_action( 'orbit_query_heading', function( $query_atts ){
 
-  $query_atts['posts_per_page'] = -1;
-
-  $orbit_wp = ORBIT_WP::getInstance();
-
-  $orbit_wp_query = $orbit_wp->query( $query_atts );
-
   $searchFlag = false;
 
-  if( isset( $orbit_wp_query->query['post_type'] ) && in_array( 'laws', $orbit_wp_query->query['post_type'] ) && isset( $orbit_wp_query->found_posts ) ){
+  if( isset( $query_atts['post_type'] ) && in_array( 'laws', $query_atts['post_type'] ) ){
 
-    $posts = wp_list_pluck( $orbit_wp_query->posts, 'ID' );
+    $orbit_wp = ORBIT_WP::getInstance();
 
-    $total_posts = $orbit_wp_query->found_posts;
+    $posts = $orbit_wp->get_post_ids( $query_atts );
+
+    $total_posts = count( $posts );
 
     if( isset( $_GET ) && count( $_GET ) ){ $searchFlag = true; }
 
     $total_count_title = 'Total ' . $total_posts . ' laws available';
 
-    if( $searchFlag ){
-      $total_count_title = 'Total ' . $total_posts . ' laws found for your query';
-    }
+    if( $searchFlag ){ $total_count_title = 'Total ' . $total_posts . ' laws found for your query'; }
 
     _e( '<div class="orbit-query-heading">' );
 
